0bf6c403fc0bf1d7dccb7cdec0ab584644e6365a
Andrew <andreyalexandrovich.k@gmail.com>
2020-12-23 19:39:10 +0200

    Step_6: created images module

---
+++ src/js/modules/images.js
@@ -0,0 +1,35 @@
+const images = () => {
+    const workSection = document.querySelector('.works'),
+          imgPopup = document.createElement('div'),
+          imgBig = document.createElement('img');
+
+    imgPopup.classList.add('popup');
+    workSection.appendChild(imgPopup);
+    imgPopup.appendChild(imgBig);
+
+    imgPopup.style.justifyContent = 'center';
+    imgPopup.style.alignItems = 'center';
+    imgPopup.style.display = 'none';
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const target = e.target;
+
+        if (target && target.classList.contains('preview')) {
+            imgPopup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+            imgBig.classList.add('mPhoto');
+
+            const path = target.parentNode.getAttribute('href');
+            imgBig.setAttribute('src', path);
+        }
+
+        if (target && target.matches('div.popup')) {
+            imgPopup.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    });
+};
+
+export default images;
